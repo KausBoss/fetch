1b83724b0604128680d2df6a23731fd1432c6d89
KausBoss <ktkaustubh4@gmail.com>
2024-03-20 18:30:27 -0700

    add models

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module receiptprocessor
 
 go 1.22.0
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
+)
---
+++ models.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/google/uuid"
+
+type Item struct {
+	ShortDescription string `json:"shortDescription"`
+	Price            string `json:"price"`
+}
+
+type Receipt struct {
+	Retailer     string    `json:"retailer"`
+	PurchaseDate string    `json:"purchaseDate"`
+	PurchaseTime string    `json:"purchaseTime"`
+	Items        []Item    `json:"items"`
+	Total        string    `json:"total"`
+	Points       int       `json:"points,omitempty"`
+	ID           uuid.UUID `json:"id,omitempty"`
+}
